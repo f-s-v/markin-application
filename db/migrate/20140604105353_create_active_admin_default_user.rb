0482143db2a545dc0480b69f16b5c31a7693ed70
Vadim Rastyagaev <rastyagaev@gmail.com>
2014-06-04 19:53:49 +0400

    fixed CreateActiveAdminDefaultUser migration

--- db/migrate/20140604105353_create_active_admin_default_user.rb
+++ db/migrate/20140604105353_create_active_admin_default_user.rb
@@ -3,7 +3,7 @@ class CreateActiveAdminDefaultUser < ActiveRecord::Migration
   def migrate(direction)
     super
     # Create a default user
-    User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if direction == :up
+    User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password', name: 'Example Admin') if direction == :up
   end
 
 end
