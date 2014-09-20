cb041f7c2f95ed3554c69ee4c963954195e639d5
Vadim Rastyagaev <rastyagaev@gmail.com>
2014-09-20 23:27:10 +0400

    see_also_products

--- app/controllers/store/products_controller.rb
+++ app/controllers/store/products_controller.rb
@@ -7,7 +7,7 @@ class Store::ProductsController < Store::BaseController
     Product.includes(:batch, options: :characteristic).where(public_id: params[:id]).first!
   end
   
-  let(:see_also_products, on: 'show') { Product.all }
+  let(:see_also_products, on: 'show') { Product.limit(3).order('random()') }
 
   def show
     respond_with resource
