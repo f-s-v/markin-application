85c5b46284124b5b41368e5b0a27fa41499eefca
Vadim Rastyagaev <rastyagaev@gmail.com>
2014-12-18 16:18:32 +0300

    updated host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_controller.default_url_options = {host: 'vm.dev.f-s-v.com'}
-  config.action_mailer.default_url_options = {host: 'vm.dev.f-s-v.com'}
+  config.action_controller.default_url_options = {host: 'vladimirmarkin.com'}
+  config.action_mailer.default_url_options = {host: 'vladimirmarkin.com'}
   config.action_mailer.delivery_method = :sendmail
 end
