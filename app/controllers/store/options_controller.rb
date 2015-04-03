9164d620982c94bb9860028726ede0e91aedf21b
Eugene Hlyzov <hlyzov@gmail.com>
2015-04-03 13:56:21 +0300

    hotfix (query was not compatible with postgresql)

--- app/controllers/store/options_controller.rb
+++ app/controllers/store/options_controller.rb
@@ -10,6 +10,7 @@ class Store::OptionsController < ApplicationController
   let(:products, on: :member) do
     resource.products.joins(:batch, :name).
       where('translations.locale' => I18n.locale).
+      select('products.*, translations.text').
       order('translations.text').
       merge(Product::Batch.searchable).uniq.to_a
   end
